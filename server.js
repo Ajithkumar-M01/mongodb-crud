aba0c64411981171e9be6b03f290dddd9e78f1f6
Ajithkumar M <ajithkumaran.srk@gmail.com>
2024-11-06 12:17:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ dotenv.config(); // Load environment variables from .env file
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: 'https://mongodb-crud-six.vercel.app', // Replace with her actual frontend URL
+  origin: 'https://mongodb-crud-six.vercel.app/', // Replace with her actual frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
